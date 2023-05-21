8a3caf71ff7533e75624d7af9be7afc5f33eae00
seanwoori <seanwoory@gmail.com>
2023-05-21 17:05:59 +0900

    fix : 앱 별 model.py 수정

--- document/backend/accounts/models.py
+++ document/backend/accounts/models.py
@@ -7,4 +7,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     points = models.IntegerField(default = 0)
     badge = models.ImageField(upload_to='badges/', blank=True, null= True)
-    
+
+
+class ViewHistory(models.Model):
+    movie_durations = models.DurationField(null=True, blank=True) # 내가 영상을 본 시간 기록
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Wishlist(models.Model):
+    movie_wishlist = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now= True)
--- document/backend/movies/views.py
+++ document/backend/movies/views.py
@@ -7,8 +7,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
-from .models import Movie, Genre
-from .serializers import MovieSerializer, MovieRandomSerializer, GenreSerializer
+from .models import Movie, Theme
+from .serializers import MovieSerializer, MovieRandomSerializer, ThemeSerializer
 
 # user 모델 가져오기
 from django.contrib.auth import get_user_model
@@ -42,26 +42,26 @@ def random(request):
     serializer = MovieRandomSerializer(movies, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-# GET : genre 데이터를 리턴
+# GET : theme 데이터를 리턴
 # POST : tinder로 받아온 선호 장르 입력
 @api_view(['GET', 'POST'])
 @authentication_classes([JSONWebTokenAuthentication])
 @permission_classes([IsAuthenticated])
-def genres(request):
+def themes(request):
     person = get_object_or_404(get_user_model(), username=request.user)
     # 해당 유저가 어떤 장르를 가장 좋아하는지 체크하기 위한 Json(dict type)
-    person_genre_dict = person.genre_dict
+    person_theme_dict = person.theme_dict
 
     if request.method == 'GET':
-        genres = Genre.objects.all()
-        serializer = GenreSerializer(genres, many=True)
+        themes = Theme.objects.all()
+        serializer = ThemeSerializer(themes, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         for data in request.data:
             # MtoM field 관리
-            person.like_genres.add(data)
+            person.like_themes.add(data)
             # Json field 관리
-            person_genre_dict[str(data)] += 1
+            person_theme_dict[str(data)] += 1
         person.save()
         return Response(status=status.HTTP_201_CREATED)
 
@@ -71,14 +71,14 @@ def genres(request):
 @permission_classes([IsAuthenticated])
 def recommend(request):
     person = get_object_or_404(get_user_model(), username=request.user)
-    person_genre_dict = person.genre_dict
+    person_theme_dict = person.theme_dict
 
     max_val = 0
-    best_genre = ''
-    for key, val in person_genre_dict.items():
+    best_theme = ''
+    for key, val in person_theme_dict.items():
         if val >= max_val:
             max_val = val
-            best_genre = key
+            best_theme = key
 
     if max_val != 0:
         movies = Movie.objects.order_by('?')[:1000]
@@ -87,14 +87,14 @@ def recommend(request):
         for movie in movies:
             if cnt >= 200:
                 break
-            if int(best_genre) in [x.id for x in movie.genre_ids.all()]:
+            if int(best_theme) in [x.id for x in movie.theme_ids.all()]:
                 recommend_movies.append(movie)
                 cnt += 1
                 continue
 
-        best_genre = get_object_or_404(Genre, pk=int(best_genre))
+        best_theme = get_object_or_404(Theme, pk=int(best_theme))
 
         serializer = MovieSerializer(recommend_movies[:50], many=True)
-        return JsonResponse({'data': serializer.data, 'best_genre': best_genre.name }, status=status.HTTP_200_OK)
+        return JsonResponse({'data': serializer.data, 'best_theme': best_theme.name }, status=status.HTTP_200_OK)
     else:
-        return JsonResponse({'best_genre': '아직 데이터가 없는 상태' }, status=status.HTTP_200_OK)
+        return JsonResponse({'best_theme': '아직 데이터가 없는 상태' }, status=status.HTTP_200_OK)
