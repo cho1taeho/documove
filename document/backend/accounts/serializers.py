b002dcfcf946daa2b86296a752e6a435273c9a57
cho1taeho <cow12dog@gmail.com>
2023-05-22 14:58:54 +0900

    22

--- document/backend/accounts/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-from .models import User
-
-class UserSerializer(serializers.ModelSerializer):
-    pass
--- document/backend/accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/points/increase/', views.increase_points_api, name='increase_points_api'),
-    path('api/points/reduce/', views.reduce_points_api, name='reduce_points_api'),
-    path('api/badge/upload/', views.upload_badge_api, name = 'upload_badge_api'),
-]
