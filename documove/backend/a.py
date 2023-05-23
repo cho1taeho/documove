40900120dc92c6f29c667652a9406caac133ca6e
cho1taeho <cow12dog@gmail.com>
2023-05-23 09:16:03 +0900

    23

--- documove/backend/a.py
+++ documove/backend/a.py
@@ -2,45 +2,45 @@ import os
 import requests
 import json
 
-def download_environment_documentaries():
-    url = 'https://api.themoviedb.org/3/discover/movie'
+def download_all_movies_by_keyword(keyword):
+    url = 'https://api.themoviedb.org/3/search/movie'
     params = {
         'api_key': 'db62ad7757737f4cf7101493c0bccdf2',
         'language': 'ko-KR',
-        'with_genres': 99,  # 다큐멘터리 장르의 genre_id
-        'page': 1,
-        'total_pages': 3000  # 원하는 페이지 수로 설정하세요
+        'query': keyword,
+        'page': 1
     }
 
-    environment_documentaries = []
+    all_movies = []
 
-    for page in range(1, params['total_pages'] + 1):
-        params['page'] = page
+    while True:
         response = requests.get(url, params=params)
         data = response.json()
         movies = data.get('results', [])
 
-        for movie in movies:
-            # 영화의 장르 목록을 가져옵니다
-            genre_ids = movie.get('genre_ids', [])
+        all_movies.extend(movies)
 
-            # "environment" 장르 ID인 10321이 포함되어 있는지 확인합니다
-            if 10321 in genre_ids:
-                environment_documentaries.append(movie)
+        # 다음 페이지로 이동
+        page = data.get('page', 1)
+        total_pages = data.get('total_pages', 1)
+        if page >= total_pages:
+            break
+        params['page'] = page + 1
 
     # 파일 경로 설정
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(base_dir, 'movies/fixtures/environment_docu.json')
+    file_path = os.path.join(base_dir, 'movies/fixtures/Environment.json')
 
     # JSON 파일로 저장
     with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(environment_documentaries, f, indent=4, ensure_ascii=False)
+        json.dump(all_movies, f, indent=4, ensure_ascii=False)
 
-    return environment_documentaries
+    return all_movies
 
 def main():
-    environment_documentaries = download_environment_documentaries()
-    print(f"환경 관련 다큐멘터리 영화 데이터를 JSON 파일로 다운로드하였습니다. (총 {len(environment_documentaries)}개)")
+    keyword = 'Environment'  # 검색할 키워드
+    movies = download_all_movies_by_keyword(keyword)
+    print(f"키워드 '{keyword}'를 포함하는 모든 영화 데이터를 JSON 파일로 다운로드하였습니다. (총 {len(movies)}개)")
 
 if __name__ == '__main__':
     main()
